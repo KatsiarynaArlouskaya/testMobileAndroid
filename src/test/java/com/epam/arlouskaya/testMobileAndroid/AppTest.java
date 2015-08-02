3275b65f306a58864e58f9c585256f00ebff6225
Katsiaryna Arlouskaya Home <katerinaorl@tut.by>
2015-08-02 14:13:26 +0300

    Add properties resourse bundle

--- src/test/java/com/epam/arlouskaya/testMobileAndroid/AppTest.java
+++ src/test/java/com/epam/arlouskaya/testMobileAndroid/AppTest.java
@@ -1,13 +1,21 @@
 package com.epam.arlouskaya.testMobileAndroid;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.epam.arlouskaya.testMobileAndroid.driverFactory.StaticFactory;
 
-import io.appium.java_client.android.AndroidDriver;
 
 /**
  * Unit test for simple App.
@@ -15,17 +23,47 @@ import io.appium.java_client.android.AndroidDriver;
 public class AppTest 
     
 {
-	private AndroidDriver driver;
-	private final String path ="";
+	private WebDriver driver;
+	private final String pathToDriverProp ="d:\\EPAM\\WorkSpace\\workspace2\\testMobileAndroid";
 	
 	@BeforeClass
 	public void setUp() throws MalformedURLException{
-		driver = StaticFactory.getAndroidDriverFromResourse(path);
+		CommandLine command = new CommandLine("cmd");
+		command.addArgument("/c");
+		command.addArgument("C://Program Files (x86)//Appium//node.exe");
+		command.addArgument("C://Program Files (x86)//Appium//node_modules//appium//bin//appium.js");
+		command.addArgument("--address");
+		command.addArgument("0.0.0.0");
+		command.addArgument("--port");
+		command.addArgument("4724");
+		command.addArgument("--no-reset");
+		command.addArgument("--log");
+		command.addArgument("C://Users//...//log//appiumLogs.txt");
+		DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
+		DefaultExecutor executor = new DefaultExecutor();
+		executor.setExitValue(1);
+		try {
+			executor.execute(command, resultHandler);
+		} catch (ExecuteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver = StaticFactory.getAndroidDriverFromResourse(pathToDriverProp);
 	}
 	
 	@Test
     public void testApp()
     {
-        
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Accessibility']")).click();
+        driver.navigate().back();
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Accessibility']")).click();
+    }
+	
+	@AfterClass
+    public void tearDown() throws Exception {
+        if (driver != null) driver.quit();
     }
 }
--- src/test/java/com/epam/arlouskaya/testMobileAndroid/driverFactory/StaticFactory.java
+++ src/test/java/com/epam/arlouskaya/testMobileAndroid/driverFactory/StaticFactory.java
@@ -1,8 +1,15 @@
 package com.epam.arlouskaya.testMobileAndroid.driverFactory;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -12,17 +19,19 @@ import io.appium.java_client.android.AndroidDriver;
 public class StaticFactory {
 	public static AndroidDriver getAndroidDriverFromResourse(String path) throws MalformedURLException{
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("appium-version", "1.1.0");
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "Android");
-        capabilities.setCapability("platformVersion", "4.3");
-        String userDir = System.getProperty("user.dir");
-
-        URL serverAddress;
-        String localApp = "api.apk";
-        String appPath = Paths.get(userDir, localApp).toAbsolutePath().toString();
-        capabilities.setCapability("app", appPath);
-        serverAddress = new URL("http://127.0.0.1:4723/wd/hub");
-		return new AndroidDriver (serverAddress, capabilities);
+		File file = new File(path);
+		URL[] urls = {file.toURI().toURL()};
+		ClassLoader loader = new URLClassLoader(urls);
+		ResourceBundle bundle = ResourceBundle.getBundle("driver", Locale.getDefault(), loader);
+		//ResourceBundle bundle = ResourceBundle.getBundle("driver");
+		Set<String> keys = bundle.keySet();
+		for (Iterator iterator = keys.iterator(); iterator.hasNext();) {
+			String key = (String) iterator.next();
+			capabilities.setCapability(key, bundle.getString(key));
+		}
+        URL serverAddress = new URL("http://127.0.0.1:4723/wd/hub");
+        AndroidDriver driver = new AndroidDriver (serverAddress, capabilities);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		return driver;
 	}
 }
